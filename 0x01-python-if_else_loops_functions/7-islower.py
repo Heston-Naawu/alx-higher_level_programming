3ac5d2bd009fb23d5802edfe6fc53f9239fd3034
HestonNaawu <contact.heston@gmail.com>
2023-11-29 05:36:58 +0000

    final

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def islower(c):
-	if (ord(c) >= ord('a') and ord(c) <= ord('z')):
-		return True
-	return False
+# Author - Tolulope Fakunle
 
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,2 @@
-Project 2
+The official Readme Document for this project
+0-positive_or_negative.py
