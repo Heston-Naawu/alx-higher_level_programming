f28fc9848f833c87e6d0942da51d08279bdd3224
HestonNaawu <contact.heston@gmail.com>
2023-12-01 01:24:23 +0000

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """ This should run when code is executed directly"""
+    """Print the sum of 1 and 2."""
     from add_0 import add
+
     a = 1
     b = 2
-    print("{} + {} = {}" .format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    
